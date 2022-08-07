f2ed9f032e32f6fd47c8364b9cb2ac6053a8e3aa
Toya <toya.dapena@xilon.es>
2022-08-07 20:01:57 +0200

    Primera entrega proyecto - Variables, array de objetos y funciones básicas

---
+++ codigo.js
@@ -0,0 +1,87 @@
+
+//Creo la clase de objeto
+class Comida {
+    constructor(nombre, referencia, categoria, precio) {
+        this.nombre = nombre;
+        this.referencia = referencia;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+}
+
+//Categorizo arrays
+const platos=[
+    {
+        nombre:"Ensalad Mixta",
+        referencia:"refA001",
+        categoria: "Entrantes",
+        precio: 6.90
+    },
+    {
+        nombre:"Ensalada César",
+        referencia:"refA002",
+        categoria: "Entrantes",
+        precio: 9.90
+    },
+    {
+        nombre:"Saquitos bolettus",
+        referencia:"refA003",
+        categoria: "Entrantes",
+        precio: 16.90
+    }
+
+];
+
+
+//Añado productos por push
+
+platos.push(new Comida("Macarrones", "refB001", "Principal", 18));
+platos.push(new Comida("Arroz con bogavante",  "refB002", "Principal", 28));
+platos.push(new Comida("Solomillo ibérico",  "refB003", "Principal",25));
+platos.push(new Comida("Tarta de fresa", "refC001", "Postres", 3));
+
+
+
+
+//Funcion que ordena los productos por orden alfabético
+function ordenAlfabetico (a,b) {
+    return (a.nombre > b.nombre);
+}
+
+console.table(platos.sort(ordenAlfabetico));
+
+
+//Funcion que ordena los productos por precio de mayor a menor
+function precioDescendente (a, b) {
+    return (b.precio - a.precio);
+}
+
+console.table(platos.sort(precioDescendente));
+
+for(const precioProducto of platos){
+    console.log("Precio: "+ precioProducto.precio);
+}
+
+
+
+let gastoEnvio = 3; //precio en euros
+
+
+const platosConImpuestos=platos.map(plato => {
+    return {
+        nombre: plato.nombre,
+        referencia: plato.referencia,
+        categoria: plato.categoria,
+        precio: plato.precio * 1.21
+    }
+});
+
+console.table(platosConImpuestos);
+
+//Calcular precio total compra
+
+//reduce
+//vamos a sumar todos los precios de los platos con Impuestos
+let totalPrecios = platosConImpuestos.reduce(((acumulador, plato)=> acumulador + plato.precio),0);
+console.log("La suma total de los platos es" ,totalPrecios + gastoEnvio);
+
